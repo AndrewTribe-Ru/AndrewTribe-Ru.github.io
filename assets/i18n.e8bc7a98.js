7089374e71a9ab26449ba07607715cc5a00fcfc2
leonid <leonid.always@gmail.com>
2025-10-31 22:44:54 +0900

    Deploy: 20251031-224454

---
+++ assets/i18n.e8bc7a98.js
@@ -0,0 +1,1 @@
+import{u as o}from"./i18n.a06c22d2.js";import"./index.295a03f5.js";var n={install:t=>{t.config.globalProperties.$t=r=>o().$t(r),t.provide("$t",r=>o().$t(r))}},u=({app:t})=>{t.use(n)};export{u as default};
