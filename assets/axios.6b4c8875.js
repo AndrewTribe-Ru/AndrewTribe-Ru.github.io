fee2f27a42d65dae68b017f90c4fd1734cdc769f
leonid <leonid.always@gmail.com>
2025-11-25 21:45:58 +0900

    Deploy: 20251125-214558

--- assets/axios.6b4c8875.js
+++
@@ -1,1 +0,0 @@
-import{b as e,a as o}from"./index.08b3f62d.js";const i=o.create({baseURL:"https://api.example.com"});var t=e(({app:a})=>{a.config.globalProperties.$axios=o,a.config.globalProperties.$api=i});export{i as api,t as default};
